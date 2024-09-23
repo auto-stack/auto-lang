b569421149fb76d8a49cdd2dc0475c3e87f34e45
zhao.puming <zhao.puming@soutek.com>
2024-09-23 11:25:10 +0800

    simple pratt parser for combinations of arithmetics

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ mod value;
 pub mod repl;
 
 pub fn run(code: &str) -> Result<String, String> {
-    // let mut lexer = lexer::Lexer::new(code);
-    // lexer.print();
+    let mut lexer = lexer::Lexer::new(code);
+    lexer.print();
     let ast = parser::Parser::new(code).parse();
-    println!("{:?}", ast);
+    println!("{}", ast);
     let value = eval::Evaler::new().eval(&ast);
     Ok(value.to_string())
 }
